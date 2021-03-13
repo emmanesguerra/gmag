d238a31ce306a839dacd210b5e61f510d5da8e4f
emmanesguerra <emman.esguerra2013@gmail.com>
2021-03-13 11:48:29 +0800

    Fix displaying of leftside bar

---
+++ resources/views/common/leftsidebar.blade.php
@@ -0,0 +1,106 @@
+<section class="side-nav">
+    <div class="logo m-3">
+        <span>
+            <img src='{{ asset('images/goal.png') }}' width='150' height="120">
+        </span>
+    </div>
+    <nav>
+        <ul>
+            <li class="active">
+                <a href="#">
+                    <span><i class="material-icons"></i></span>
+                    <span style="color:#900; font-weight:bold; font-size:16px;">MAIN NAVIGATION</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <span><i class="fas fa-tachometer-alt"></i></span>
+                    <span>Dashboard</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <span><i class="fa fa-sitemap"></i></span>
+                    <span>Network</span>
+                </a>
+                <ul>
+                    <li><a href="#"><i class="fa fa-ellipsis-h"></i> Genealogy Tree</a></li>
+                    <li><a href="#"><i class="fa fa-ellipsis-h"></i>Binary List</a>
+                    </li><li><a href="#"><i class="fa fa-ellipsis-h"></i>D/I Genealogy List</a>
+                    </li> 
+                </ul>
+            </li>
+            <li>
+                <a href="#">
+                    <span><i class="fa fa-bar-chart"></i></span>
+                    <span>Commissions</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <span><i class="fa fa-envelope"></i></span>
+                    <span>Mail</span>
+                </a>
+                <ul>
+                    <li><a href="#"><i class="fa fa-ellipsis-h"></i> Code Vault</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">
+                    <span><i class="fa fa-shopping-bag"></i></span>
+                    <span>Product Unilevel</span>
+                </a>
+                <ul>
+                    <li><a href="#"><i class="fa fa-ellipsis-h"></i> Product Activation</a>
+                    </li>
+                    <li><a href="#"><i class="fa fa-ellipsis-h"></i> Purchase Summary</a>
+                    </li>
+
+                </ul> 
+            </li>
+            <li>
+                <a href="#">
+                    <span><i class="fas fa-wallet"></i></span>
+                    <span>E-Wallet</span>
+                </a>
+                <ul>
+                    <li><a href="#"><i class="fa fa-ellipsis-h"></i> My E-Wallet</a>
+                    </li>
+                    <li><a href="#"><i class="fa fa-ellipsis-h"></i> Transactions History</a>
+                    </li>
+
+                </ul>  
+            </li>
+            <li>
+                <a href="#">
+                    <span><i class="fas fa-cogs"></i></span>
+                    <span>Settings</span>
+                </a>
+                <ul>
+                    <li><a href="#"><i class="fa fa-ellipsis-h"></i> Switch Account</a></li>
+                    <li><a href="#"><i class="fa fa-ellipsis-h"></i> Edit Profile</a></li>
+                    <li><a href="#"><i class="fa fa-ellipsis-h"></i> Change Password</a></li>
+
+                </ul>  
+            </li>
+            <li> 
+                <a href="#">
+                    <span><i class="fa fa-certificate"></i></span>
+                    <span>COURSES</span>
+                </a>
+            </li>
+            <li>
+                <a href="#" onclick="event.preventDefault();
+                                               document.getElementById('logout-form').submit();">
+                    <span><i class="fa fa-power-off"></i></span>
+                    <span>Logout</span>
+                </a>
+                
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
+            </li>
+        </ul>
+    </nav>
+</section>
---
+++ resources/views/layouts/dashboard.blade.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    @yield('title')
+    
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:700, 600,500,400,300" rel="stylesheet" type="text/css">
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body style="background-color: #d5dae5; font-family: 'Source Sans Pro', sans-serif; line-height: 1.15;">
+    <div class="dashboard">
+        <main>
+            <div class="container p-0">
+                @include('common.leftsidebar')
+
+                @yield('module-content')
+            </div>
+        </main>
+    </div>
+</body>
+</html>
