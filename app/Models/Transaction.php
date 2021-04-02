667bbab6245b8a4844177bf3cd61697507a8ffb3
emmanesguerra <emman.esguerra2013@gmail.com>
2021-04-02 23:32:31 +0800

    add audit trail to transaction and transaction bonuses

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -3,8 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class Transaction extends Model
+class Transaction extends Model implements Auditable
 {
-    //
+    use \OwenIt\Auditing\Auditable;
+
+    protected $fillable = [
+        'member_id', 'product_id', 'price', 'transaction_date'
+    ];
+    
+    /*
+     * For audit tags
+     */
+    protected $auditExclude = [
+        'created_by',
+        'updated_by'
+    ];
+    public function generateTags(): array
+    {
+        return ['displayToDashboard'];
+    }
 }
--- app/Models/TransactionBonus.php
+++ app/Models/TransactionBonus.php
@@ -3,8 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class TransactionBonus extends Model
+class TransactionBonus extends Model implements Auditable
 {
+    use \OwenIt\Auditing\Auditable;
+    
     protected $fillable = ['member_id', 'class_id', 'class_type', 'type', 'acquired_amt'];
+    
+    /*
+     * For audit tags
+     */
+    protected $auditExclude = [
+        'created_by',
+        'updated_by'
+    ];
+    public function generateTags(): array
+    {
+        return ['displayToDashboard'];
+    }
 }
