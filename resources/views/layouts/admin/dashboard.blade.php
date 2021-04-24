d2afda75bbdedd28ad55a96e611bb407429db458
emmanesguerra <emman.esguerra2013@gmail.com>
2021-04-24 16:24:26 +0800

    reduce page loading by hosting jquery and fontawesome icons

--- resources/views/layouts/admin/dashboard.blade.php
+++ resources/views/layouts/admin/dashboard.blade.php
@@ -15,8 +15,9 @@
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:700, 600,500,400,300" rel="stylesheet" type="text/css">
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    
+    <!-- For font awesome -->
+    <link href="{{ asset('css/all.min.css') }}" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
