6eccd700d20170d0d5f3e0a39794aa6c740e10dc
emmanesguerra <emman.esguerra2013@gmail.com>
2021-04-24 14:11:51 +0800

    add relationship for tranasaction bonuses; remove pairing relationship, instead create scope

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -58,30 +58,6 @@ class Member extends Authenticatable implements Auditable
         return $this->hasMany(MembersPairing::class, 'member_id', 'id');
     }
     
-    public function pairingsMP()
-    {
-        return $this->hasMany(MembersPairing::class, 'member_id', 'id')
-                ->where('type', 'MP');
-    }
-    
-    public function pairingsFP()
-    {
-        return $this->hasMany(MembersPairing::class, 'member_id', 'id')
-                ->where('type', 'FP');
-    }
-        
-    public function pairingsToday()
-    {
-        return $this->hasMany(MembersPairing::class, 'member_id', 'id')
-                ->whereDate('created_at', date('Y-m-d'));
-    }
-    
-    public function pairingsYesterday()
-    {
-        return $this->hasMany(MembersPairing::class, 'member_id', 'id')
-                ->whereDate('created_at', date("Y-m-d", time() - 3600*24));
-    }
-    
     public function entry_code()
     {
         return $this->hasOne(RegistrationCode::class, 'id', 'registration_code_id');
@@ -95,5 +71,10 @@ class Member extends Authenticatable implements Auditable
     public function pair_cycle()
     {
         return $this->hasOne(MembersPairCycle::class, 'id', 'pair_cycle_id');
+    }
+    
+    public function transactionBonuses()
+    {
+        return $this->hasMany(TransactionBonus::class, 'member_id', 'id');
     }
 }
