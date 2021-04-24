be8cb8da752d350dc3ef63f41f9c9671e8ab1cae
emmanesguerra <emman.esguerra2013@gmail.com>
2021-04-24 14:13:08 +0800

    fix displaying of data in dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -29,12 +29,30 @@ class HomeController extends Controller
         
         $counts = [
             'tpc' => $member->pairings->count(),
-            'mspc' => $member->pairingsMP->count(),
-            'fpc' => $member->pairingsFP->count(),
-            'tspc' => $member->pairingsToday->count(),
-            'yspc' => $member->pairingsYesterday->count()
+            'mspc' => $member->pairings()->ofType('MP')->count(),
+            'fpc' => $member->pairings()->ofType('FP')->count(),
+            'tspc' => $member->pairings()->whenDate(date('Y-m-d'))->count(),
+            'yspc' => $member->pairings()->whenDate(date("Y-m-d", time() - 3600*24))->count()
         ];
         
         return view('home', ['member' => $member, 'counts' => $counts]);
+    }
+    
+    public function earnings(Request $request)
+    {
+        $member = Member::find($request->member_id);
+        
+        $range = [
+              $request->start." 00:00:00", $request->end." 23:59:59"  
+        ];
+        
+        return response()->json([
+            'te' => $member->transactionBonuses()->ofNotType('FP')->whereBetween('created_at', $range)->sum('acquired_amt'),
+            'eb' => $member->transactionBonuses()->ofType('EB')->whereBetween('created_at', $range)->sum('acquired_amt'),
+            'mp' => $member->transactionBonuses()->ofType('MP')->whereBetween('created_at', $range)->sum('acquired_amt'),
+            'dr' => $member->transactionBonuses()->ofType('DR')->whereBetween('created_at', $range)->sum('acquired_amt'),
+            'fp' => $member->transactionBonuses()->ofType('FP')->whereBetween('created_at', $range)->sum('acquired_amt'),
+            'success' => true
+        ], 200);
     }
 }
