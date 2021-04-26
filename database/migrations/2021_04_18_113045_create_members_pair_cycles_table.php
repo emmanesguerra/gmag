9655bf77590d5da3576503cb66692b4b4b8c911c
emmanesguerra <emman.esguerra2013@gmail.com>
2021-04-26 22:24:35 +0800

    add new fields

--- database/migrations/2021_04_18_113045_create_members_pair_cycles_table.php
+++ database/migrations/2021_04_18_113045_create_members_pair_cycles_table.php
@@ -18,12 +18,16 @@ class CreateMembersPairCyclesTable extends Migration
             $table->unsignedBigInteger('member_id');
             $table->date('start_date');
             $table->date('end_date')->nullable();
+            $table->unsignedBigInteger('starting_pair_id')->nullable();
+            $table->unsignedBigInteger('ending_pair_id')->nullable();
             $table->unsignedBigInteger('product_id');
             $table->unsignedSmallInteger('max_pair');
             $table->timestamps();
             
             $table->foreign('member_id')->references('id')->on('members');
             $table->foreign('product_id')->references('id')->on('products');
+            $table->foreign('starting_pair_id')->references('id')->on('members_pairings');
+            $table->foreign('ending_pair_id')->references('id')->on('members_pairings');
         });
     }
 
