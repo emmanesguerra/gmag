45fbca262f33c39b778a566410a0017e0e457960
Emmanuelle Esguerra <emman.esguerra2013@gmail.com>
2021-05-05 13:12:04 +0800

    add link_id and description field

--- database/migrations/2021_05_03_152420_create_courses_table.php
+++ database/migrations/2021_05_03_152420_create_courses_table.php
@@ -16,8 +16,10 @@ class CreateCoursesTable extends Migration
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
             $table->string('title', 50);
+            $table->text('description')->nullable();
             $table->char('source', 1)->comment('1 = 3rd Party Program, 2 = File Upload');
-            $table->string('link', 50)->nullable();
+            $table->string('link', 250)->nullable();
+            $table->string('link_id', 50)->nullable();
             $table->string('filename', 50)->nullable();
             $table->unsignedBigInteger('created_by')->nullable();
             $table->unsignedBigInteger('updated_by')->nullable();
