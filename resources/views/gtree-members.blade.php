6b6e65d63243f540eefcd91222d61205a64c67f1
emmanesguerra <emman.esguerra2013@gmail.com>
2021-05-08 14:09:19 +0800

    remove target=blank

--- resources/views/gtree-members.blade.php
+++ resources/views/gtree-members.blade.php
@@ -14,7 +14,7 @@
     @endif
 @else
     @if ($data['username'] == 'Sign Up')
-    <a href='{{ route('register.member.create', ['target_id' => $data['target_id'], 'position' => $data['position']]) }}' target="_blank" class='gtree-a'>
+    <a href='{{ route('register.member.create', ['target_id' => $data['target_id'], 'position' => $data['position']]) }}' class='gtree-a'>
     @endif
 @endif
 <img class='gtree-img' src='{{ asset('images/' . $data['display_icon'] ) }}' />
