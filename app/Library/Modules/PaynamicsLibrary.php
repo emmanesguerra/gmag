bb044fe9273457402f3051fa5c92d677f332f928
emmanesguerra <emman.esguerra2013@gmail.com>
2021-05-08 10:47:28 +0800

    intial commit of paynamics lib

---
+++ app/Library/Modules/PaynamicsLibrary.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace App\Library\Modules;
+
+/**
+ * Description of PaynamicsLibrary
+ *
+ * @author alvin
+ */
+class PaynamicsLibrary {
+    //put your code here
+    
+    public static function makeTransaction()
+    {
+        return true;
+    }
+}
