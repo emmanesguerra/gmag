69f0386bdf4081cf9aaa6699392d7c0208c7e6ab
emmanesguerra <emman.esguerra2013@gmail.com>
2021-05-09 14:57:40 +0800

    add calendar filters on list

--- app/Http/Controllers/Admin/TransactionsController.php
+++ app/Http/Controllers/Admin/TransactionsController.php
@@ -120,6 +120,18 @@ class TransactionsController extends Controller
                                                 b.firstname,
                                                 b.lastname")
                             );
+        
+        if($request->has('status') && !empty($request->status)) {
+            $filteredmodel->where('a.type', $request->status);
+        }
+        
+        if($request->has('start_date') && !empty($request->start_date)) {        
+            if($request->has('end_date') && !empty($request->end_date && $request->start_date != $request->end_date)) {
+                $filteredmodel->whereBetween('a.created_at', [$request->start_date, $request->end_date . ' 23:59:00']);
+            } else {
+                $filteredmodel->whereDate('a.created_at', $request->start_date);
+            }
+        }
 
         $modelcnt = $filteredmodel->count();
 
--- resources/views/admin/transaction/bonus.blade.php
+++ resources/views/admin/transaction/bonus.blade.php
@@ -26,20 +26,57 @@
 @endsection
 
 @section('css')
+    <link href="{{ asset('css/daterangepicker.css') }}"  rel="stylesheet">
     <link rel="stylesheet"  href="{{ asset('js/DataTables/datatables.min.css') }}" />
+    <style>
+        div.dataTables_length,
+        div.dataTables_info,
+        div.paging_simple_numbers,
+        div.dataTables_filter {
+            float: right;
+        }
+        div.dataTables_info,
+        div.paging_simple_numbers {
+            width: 50%;
+        }
+        div.dataTables_length {
+            float: left;
+            width: 20%;
+        }
+        div.toolbar.dataTables_filter {
+            width: 55%;
+            float: left;
+        }
+        div.toolbar select {
+            width: auto;
+            display: inline-block;
+        }
+    </style>
 @endsection
 
 @section('javascripts')
     <script src="{{ asset('js/moment.js') }}"></script>
+    <script src="{{ asset('js/daterangepicker.js') }}"></script>
+    <script src="{{ asset('js/daterange.js') }}"></script>
     <script type="text/javascript" src="{{ asset('js/DataTables/datatables.min.js') }}"></script>
     <script>
-        $('#transaction-table').DataTable({
+        
+        var start = moment('{{ env('GO_LIVE') }}');
+        var end = moment();
+        
+        var table = $('#transaction-table').DataTable({
             "ajax": {
-                "url": "{{ route('admin.transactions.bonusdata') }}"
+                "url": "{{ route('admin.transactions.bonusdata') }}",
+                data: function ( d ) {
+                    d.status = $('#status').val();
+                    d.start_date = start.format('Y-MM-DD');
+                    d.end_date = end.format('Y-MM-DD');
+                }
             },
             serverSide: true,
             responsive: true,
             processing: true,
+            "dom": 'l<"toolbar dataTables_filter">frtpi',
             "columns": [
                 {
                     data: function ( row, type, set ) {
@@ -88,5 +125,23 @@
             ],
             "order": [[ 0, "desc" ]]
         });
+        
+        $("div.toolbar").html(
+            '<label style="float: left;">Display <select id="status" class="custom-select custom-select-sm" onChange="drawTable()">'+
+                '<option value="">All</option>'+
+                '<option value="DR">Direct Referral</option>'+
+                '<option value="EB">Encoding Bonus</option>'+
+                '<option value="MP">Matching Pair</option>'+
+                '<option value="FP">Flush Pair</option>'+
+            '</select></label>' +
+            '<div id="reportrange" class="btn" style="margin-top: -4px">'+
+                '<i class="fa fa-calendar"></i>&nbsp;'+
+                '<span></span> <i class="fa fa-caret-down"></i>'+
+            '</div>'
+        );
+    
+        function drawTable() {
+            table.ajax.reload();
+        }
     </script>
 @endsection
