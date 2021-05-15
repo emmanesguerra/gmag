735fc8df5dc6ef89106da56bc718f7d6574f3966
emmanesguerra <emman.esguerra2013@gmail.com>
2021-05-15 14:23:17 +0800

    add docu for dashboard controller; add lifetime option for date-range

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Member;
 
+/**
+ * @group Member's Dashboard
+ *
+ */
 class HomeController extends Controller
 {
     /**
@@ -19,7 +23,7 @@ class HomeController extends Controller
     }
 
     /**
-     * Show the application dashboard.
+     * Display member's application dashboard
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
@@ -38,40 +42,38 @@ class HomeController extends Controller
         return view('home', ['member' => $member, 'counts' => $counts]);
     }
     
+
+    /**
+     * Updates the dashboard earnings depends on the selected date range.
+     *
+     * @queryParam start timestamp required
+     * Starting date. Example: 2021-05-01
+     *
+     * @queryParam end timestamp required
+     * Ending date. Example: 2021-05-30
+     * 
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
     public function earnings(Request $request)
     {
         $member = Member::find($request->member_id);
         
-        if($request->start == date('Y-m-d') && $request->end == date('Y-m-d')) {
-            
-            return response()->json([
-                'curr' => $member->total_amt,
-                'dr' => $member->direct_referral,
-                'eb' => $member->encoding_bonus,
-                'mp' => $member->matching_pairs,
-                'ewallet_purchased' => $member->purchased,
-                'te' => $member->total_amt + $member->purchased,
-                'fp' => $member->flush_pts,
-                'success' => true
-            ], 200);
-        } else {
-            $range = [
-                  $request->start." 00:00:00", $request->end." 23:59:59"  
-            ];
-            
-            $ewallet = $member->transactions()->ofPaymentMethod('e_wallet')->whereBetween('transaction_date', $range)->sum('product_price');
-            $total_earnings = $member->transactionBonuses()->ofNotType('FP')->whereBetween('created_at', $range)->sum('acquired_amt');
+        $range = [
+              $request->start." 00:00:00", $request->end." 23:59:59"  
+        ];
 
-            return response()->json([
-                'curr' => $total_earnings - $ewallet,
-                'dr' => $member->transactionBonuses()->ofType('DR')->whereBetween('created_at', $range)->sum('acquired_amt'),
-                'eb' => $member->transactionBonuses()->ofType('EB')->whereBetween('created_at', $range)->sum('acquired_amt'),
-                'mp' => $member->transactionBonuses()->ofType('MP')->whereBetween('created_at', $range)->sum('acquired_amt'),
-                'ewallet_purchased' => $ewallet,
-                'te' => $total_earnings,
-                'fp' => $member->transactionBonuses()->ofType('FP')->whereBetween('created_at', $range)->sum('acquired_amt'),
-                'success' => true
-            ], 200);
-        }
+        $ewallet = $member->transactions()->ofPaymentMethod('e_wallet')->whereBetween('transaction_date', $range)->sum('product_price');
+        $total_earnings = $member->transactionBonuses()->ofNotType('FP')->whereBetween('created_at', $range)->sum('acquired_amt');
+
+        return response()->json([
+            'curr' => $total_earnings - $ewallet,
+            'dr' => $member->transactionBonuses()->ofType('DR')->whereBetween('created_at', $range)->sum('acquired_amt'),
+            'eb' => $member->transactionBonuses()->ofType('EB')->whereBetween('created_at', $range)->sum('acquired_amt'),
+            'mp' => $member->transactionBonuses()->ofType('MP')->whereBetween('created_at', $range)->sum('acquired_amt'),
+            'ewallet_purchased' => $ewallet,
+            'te' => $total_earnings,
+            'fp' => $member->transactionBonuses()->ofType('FP')->whereBetween('created_at', $range)->sum('acquired_amt'),
+            'success' => true
+        ], 200);
     }
 }
