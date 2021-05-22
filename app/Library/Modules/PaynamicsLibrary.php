d1842b818cd1fdf12dcaf4827b2644846c2cd4f2
Emmanuelle Esguerra <emman.esguerra2013@gmail.com>
2021-05-22 12:53:35 +0800

    add paynamics gateway urls on library

--- app/Library/Modules/PaynamicsLibrary.php
+++ app/Library/Modules/PaynamicsLibrary.php
@@ -15,8 +15,47 @@ namespace App\Library\Modules;
  */
 class PaynamicsLibrary {
     //put your code here
+    protected $dGateDisbursementServiceUrl;
+    protected $dGateDisbursementQueryUrl;
+    protected $dGateDisbursementCancelUrl;
+    protected $dGateDisbursementRetryUrl;
+    protected $dGateDisbursementRetriveBalanceUrl;
+    protected $dGateDisbursementRetrieveTrxnHistoryUrl;
+    protected $dGateDisbursementRetrieveProcessorStatusUrl;
+                
+    protected $alphaOneDGateDisbursementUrl;
+    protected $alphaOneDGateDisbursementQueryUrl;
+    protected $alphaOneDGateDisbursementCancelUrl;
     
-    public static function makeTransaction()
+    public function __construct() {
+        if(env('PYNMCS_ENV') == 'production') {
+            $this->dGateDisbursementServiceUrl = "";
+            $this->dGateDisbursementQueryUrl = "";
+            $this->dGateDisbursementCancelUrl = "";
+            $this->dGateDisbursementRetryUrl = "";
+            $this->dGateDisbursementRetriveBalanceUrl = "";
+            $this->dGateDisbursementRetrieveTrxnHistoryUrl = "";
+            $this->dGateDisbursementRetrieveProcessorStatusUrl = "";
+                
+            $this->alphaOneDGateDisbursementUrl = "";
+            $this->alphaOneDGateDisbursementQueryUrl = "";
+            $this->alphaOneDGateDisbursementCancelUrl = "";
+        } else {
+            $this->dGateDisbursementServiceUrl = "https://testpti.payserv.net/DisbursementGateTest/DisbursementImpl.svc/DisbursementServiceV2";
+            $this->dGateDisbursementQueryUrl = "https://testpti.payserv.net/DisbursementGateTest/DisbursementImpl.svc/DisbursementQuery";
+            $this->dGateDisbursementCancelUrl = "https://testpti.payserv.net/DisbursementGateTest/DisbursementImpl.svc/DisbursementCancel";
+            $this->dGateDisbursementRetryUrl = "https://testpti.payserv.net/DisbursementGateTest/DisbursementImpl.svc/RetryDisbursementV2";
+            $this->dGateDisbursementRetriveBalanceUrl = "https://testpti.payserv.net/DisbursementGateTest/DisbursementImpl.svc/RetrieveBalance";
+            $this->dGateDisbursementRetrieveTrxnHistoryUrl = "https://testpti.payserv.net/DisbursementGateTest/DisbursementImpl.svc/RetrieveTrxnHistory";
+            $this->dGateDisbursementRetrieveProcessorStatusUrl = "https://testpti.payserv.net/DisbursementGateTest/DisbursementImpl.svc/RetrieveProcessorStatus";
+                
+            $this->alphaOneDGateDisbursementUrl = "https://testpti.payserv.net/DisbursementGateTest/AlphaoneImpl.svc/Disbursement";
+            $this->alphaOneDGateDisbursementQueryUrl = "https://testpti.payserv.net/DisbursementGateTest/DisbursementImpl.svc/DisbursementQuery";
+            $this->alphaOneDGateDisbursementCancelUrl = "https://testpti.payserv.net/DisbursementGateTest/DisbursementImpl.svc/DisbursementCancel";
+        }
+    }
+    
+    public static function makeTransaction($request = null, string $type = '')
     {
         return true;
     }
