3c620bb392bef349246230fe115c98d33c793f24
Emmanuelle Esguerra <emman.esguerra2013@gmail.com>
2021-06-07 09:46:47 +0800

    check if remarks is set first before splitting

--- resources/views/admin/registrationcodes/used.blade.php
+++ resources/views/admin/registrationcodes/used.blade.php
@@ -97,7 +97,7 @@
                 {
                     data: "remarks",
                     render: function ( data, type, set ) {
-                        return data.split("\\n").join("<br/>");
+                        return (data) ? data.split("\\n").join("<br/>"): '';
                     }
                 }
             ],
